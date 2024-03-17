6068cf2fae2ec26a92e4e046c2edc076d89a6866
Johan Swan <jswannieman@gmail.com>
2024-03-17 19:35:07 +1300

    Commit index JS Component

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require("express");
 var app = express();
 
@@ -10,13 +6,9 @@ var app = express();
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use("public", express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
