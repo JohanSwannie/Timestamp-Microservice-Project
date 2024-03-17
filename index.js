dd9582deaed655b24cf5968354e98544999bbe24
Johan Swan <jswannieman@gmail.com>
2024-03-17 20:31:57 +1300

    Commit index JS Component

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.use("public", express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
@@ -15,7 +13,13 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// Listen on port set in environment variable or default to 3000
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
